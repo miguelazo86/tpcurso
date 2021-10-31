3a44cd00b6bd8a03a0066ef258e26f04d293bab1
miguelazo86 <92006873+miguelazo86@users.noreply.github.com>
2021-10-31 02:04:03 -0300

    Update backe.js

--- backe.js
+++ backe.js
@@ -1,8 +1,9 @@
+var valorEntrada = 200;
 
 function resumen() {
     var cantidadEntradas = document.getElementById("cantidadentradas").value;
     var porcentaje = document.getElementById("porcentajedescuento").value;
-    var subTotal =(cantidadEntradas * 200);
+    var subTotal =(cantidadEntradas * valorEntrada);
     var descuento = (subTotal * porcentaje)/100;
     var totalFinal =(subTotal - descuento); 
     document.getElementById("totalprice").value=("Total a pagar: $ " + totalFinal);
